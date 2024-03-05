79198c14475b0f587dadf24ea6293387a7bf6a98
aswds <sasha.iilllnitskiy@gmail.com>
2024-03-05 16:02:55 +0200

    [fix] - bug fix

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useSetHeaderHeight } from "@/hooks/useSetHeaderHeight";
 import { NavbarLinks } from "@/links";
-import { ComponentRef, useLayoutEffect, useRef, useState } from "react";
+import { ComponentRef, useRef, useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import { MdClose } from "react-icons/md";
 import { Link } from "react-router-dom";
@@ -10,7 +10,8 @@ const Navbar = () => {
   const headerRef = useRef<ComponentRef<"div">>(null); // Create a ref to store the header element
 
   useSetHeaderHeight(headerRef);
-  async function handleMobileMenu() {
+  async function handleMobileMenu(e: React.MouseEvent) {
+    e.preventDefault();
     setShowMobileMenu((showMenu) => !showMenu);
 
     const body = document.querySelector("body");
@@ -20,7 +21,6 @@ const Navbar = () => {
       body?.classList.remove("is-menu-open");
     }
   }
-  useLayoutEffect(() => {}, []);
   return (
     <header className={`navbar-container`} ref={headerRef}>
       <nav className={`navbar ${showMobileMenu ? "is-menu-open" : ""} `}>
@@ -29,7 +29,7 @@ const Navbar = () => {
         </a>
         <div className="linksContainer">
           {NavbarLinks.map((link) => (
-            <Link key={link.title} className="link" to={""}>
+            <Link key={link.title} className="link" to={link.path}>
               {link.title}
             </Link>
           ))}
